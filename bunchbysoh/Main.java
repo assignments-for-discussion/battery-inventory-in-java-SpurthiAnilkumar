aa64324d14422e298ff00f12baf89c7e6de698a7
Spurthi Anilkumar <144123328+SpurthiAnilkumar@users.noreply.github.com>
2023-09-12 13:16:24 +0530

    Update Main.java

--- bunchbysoh/Main.java
+++ bunchbysoh/Main.java
@@ -1,28 +1,42 @@
 package bunchbysoh;
 
 public class Main {
-  static class CountsBySoH {
-    public int healthy = 0;
-    public int exchange = 0;
-    public int failed = 0;
-  };
+  public static void main(String[] args){
+    double[] capacities={115,118,80,95,91,72};
+    int healthy = 0;
+    int exchange = 0;
+    int failed = 0;
 
-  static CountsBySoH countBatteriesByHealth(int[] presentCapacities) {
+    for(double capacity: capacities){
+      double initialcapacity=100.0;
+      double soh=(capacity/initialcapacity)*100.0;
+      if(soh>80.0){
+        healthycount++;
+      }
+      else if(soh>=63.0){
+        exchangecount++;
+      }
+      else{
+        failedcount++;
+      }
+ /* static CountsBySoH countBatteriesByHealth(int[] presentCapacities) {
     CountsBySoH counts = new CountsBySoH();
     return counts;
-  }
-
-  static void testBucketingByHealth() {
+  }*/
     System.out.println("Counting batteries by SoH...\n");
-    int[] presentCapacities = {115, 118, 80, 95, 91, 72};
+    System.out.println("Number of Healthy Batteries:"+healthycount);
+    System.out.println("Number of Exchange Batteries:"+exchangecount);
+    System.out.println("Number of Failed Batteries:"+failedcount);
+    }
+  }
+   /* int[] presentCapacities = {115, 118, 80, 95, 91, 72};
     CountsBySoH counts = countBatteriesByHealth(presentCapacities);
     assert(counts.healthy == 2);
     assert(counts.exchange == 3);
     assert(counts.failed == 1);
     System.out.println("Done counting :)\n");
-  }
+  }*/
 
-  public static void main(String[] args) {
-    testBucketingByHealth();
-  }
-}
+  //public static void main(String[] args) {
+    //testBucketingByHealth();
+  
